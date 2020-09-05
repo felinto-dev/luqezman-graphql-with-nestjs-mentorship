a97570595ef1b81401834220e1bafd6e86688f1b
Felinto <emersong20.email@gmail.com>
2020-09-04 22:27:49 -0300

    feat: remove support for environment variables using 'config' npm package

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,15 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as config from 'config'
-
-const dbConfig = config.get('db')
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: dbConfig.type,
-  host: process.env.DB_HOST || dbConfig.host,
-  port: process.env.DB_PORT || dbConfig.port,
-  username: process.env.DB_USERNAME || dbConfig.username,
-  password: process.env.DB_PASSWORD || dbConfig.password,
-  database: process.env.DB_NAME || dbConfig.database,
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: false,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
-
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || serverConfig.port);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
