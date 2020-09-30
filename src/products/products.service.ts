ee5a0ad3f86ee8a6063c79afc8bdcaf2c3c299ba
Felinto <emersong20.email@gmail.com>
2020-09-30 20:36:27 -0300

    ✨ add products service

---
+++ src/products/products.service.ts
@@ -0,0 +1,13 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product, ProductDocument } from './models/product.schema';
+
+@Injectable()
+export class ProductsService {
+  constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) { }
+
+  async findAll(): Promise<Product[]> {
+    return this.productModel.find().exec();
+  }
+}
