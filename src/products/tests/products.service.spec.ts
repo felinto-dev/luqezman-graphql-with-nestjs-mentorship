e12c82406b524472658a16df0f7afe71f839921f
Felinto <emersong20.email@gmail.com>
2020-09-30 21:21:22 -0300

    🚚 create a new "tests" folder

--- src/products/tests/products.service.spec.ts
+++ src/products/tests/products.service.spec.ts
@@ -1,7 +1,7 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { ProductsService } from './products.service';
+import { ProductsService } from '../products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
