713311547269bbae373e661f58810ec7347858f2
Felinto <emersong20.email@gmail.com>
2020-10-12 15:56:30 -0300

    🔥 remove controller (rest api)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import appConfig from './config/app/app.config';
 import { DbConfigModule } from './config/db/db-config.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development',
@@ -23,6 +20,5 @@ import { ProductsModule } from './products/products.module';
     }),
     ProductsModule,
   ],
-  providers: [AppService],
 })
 export class AppModule { }
