904890e6cffa1f58f4faa85eae05676794145d21
Felinto <emersong20.email@gmail.com>
2020-10-21 10:19:59 -0300

    🏗 use lowerUppercase instead Uppercase for enum keys

--- src/products/types/product-type.enum.ts
+++ src/products/types/product-type.enum.ts
@@ -1,5 +1,5 @@
 export enum ProductType {
-  PLUGIN = 'plugin',
-  THEME = 'theme',
-  ELEMENTOR_TEMPLATE = 'elementor_template',
+  Plugin = 'plugin',
+  Theme = 'theme',
+  ElementorTemplate = 'elementorTemplate',
 }
